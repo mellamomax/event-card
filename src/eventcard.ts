92daa821231ea88bb1e2b5a21c30e5d33b73e73c
Mellamomax <max.skold@hotmail.com>
2025-01-13 21:16:19 +0100

    s

--- src/eventcard.ts
+++ src/eventcard.ts
@@ -1,6 +1,10 @@
 import { version } from '../package.json';
+import { EventCard } from './cards/event-card/event-card';
 
 // eslint-disable-next-line no-console
 console.info(`%c🗑️ EventCard ${version}`, 'background-color: #ef5350; color: #ffffff');
 
-export { EventCard } from './cards/event-card/event-card';
+// Ensure the custom element is registered
+customElements.define('event-card', EventCard);
+
+export { EventCard };
