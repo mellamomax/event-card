50f59a4777cee02a487bd04dc9cdb14990cd7d24
Mellamomax <max.skold@hotmail.com>
2025-02-04 23:25:50 +0100

    none

--- src/utils/getDateString.ts
+++ src/utils/getDateString.ts
@@ -7,8 +7,8 @@ import type { TrashCardConfig } from '../cards/trash-card/trash-card-config';
 import type { HomeAssistant } from './ha';
 import type { CalendarItem } from './calendarItem';
 
-const format = (date: Date, dateStyleFormat: string, hass.languageuage: string) =>
-  DateTime.fromJSDate(date).setLocale(hass.languageuage).toFormat(dateStyleFormat);
+const format = (date: Date, dateStyleFormat: string, hass.language: string) =>
+  DateTime.fromJSDate(date).setLocale(hass.language).toFormat(dateStyleFormat);
 
 const getTimeString = (customLocalize, offset: string, day?: string, startTime?: string, endTime?: string, excludeTime?: boolean, short?: boolean) => {
   if (offset === 'today' || offset === 'tomorrow') {
@@ -28,13 +28,13 @@ const getDateString = (
   dayStyle?: TrashCardConfig['day_style'],
   dayStyleFormat?: TrashCardConfig['day_style_format'],
   hass?: HomeAssistant,
-  hass.languageuageOverride?: string
+  hass.languageOverride?: string
 ): string => {
   if (!hass) {
     return '';
   }
   
-  const hass.language = hass.languageuageOverride || hass.hass.languageuage;
+  const hass.language = hass.languageOverride || hass.hass.language;
   const customLocalize = setupCustomlocalize(hass);
 
   const today = new Date();
